ff597579cab0268ecd2226f97245c43b0440af0c
Kayanr <kayan.reid@gmail.com>
2021-02-07 14:53:43 -0500

    Add formatting and comments
    Add formatting and comments

--- pokemonRandom/app.js
+++ pokemonRandom/app.js
@@ -1,6 +1,7 @@
 let plist = document.getElementById("listPokemon");
 
 function getPokemon(id, num) {
+  //For example pickachu's id is 25 so url would be: https://pokeapi.co/api/v2/pokemon/25
   fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(function (response) {
     response.json().then(function (pokemon) {
       //console.log(pokemon.name);
@@ -10,6 +11,8 @@ function getPokemon(id, num) {
 }
 
 function getPokemons() {
+  //There are more pokemon than I thought but for this purpose there are 150 pokemons in a pokedex. 
+  //Randomly getting 2 ids of pokemon
   let firstId = Math.round(Math.random() * 150);
   let secondId = Math.round(Math.random() * 150);
 
@@ -20,9 +23,15 @@ function getPokemons() {
 function createPokemon(pokemon, num) {
   let item = plist.querySelector(`#pokemon-${num}`);
   let pimage = item.getElementsByTagName("img")[0];
-  pimage.setAttribute("src", pokemon.sprites.front_default);
+ 
+  //For example, Pickachu image is https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png
+  //pimage.setAttribute("src", pokemon.sprites.front_default);
+  //Using what instructors showed us instead of the above with setAttribute
+  pimage.src = pokemon.sprites.front_default;
 
   let pnum = item.getElementsByTagName("p")[0];
-  pnum.innerHTML = `<h1>Name: ${pokemon.name}</h1> <h2> ID: #${pokemon.id}</h2> <h3> Type: ${pokemon.types[0].type.name} ${pokemon.types[1]? ", "+pokemon.types[1].type.name : " "}<\h3>`;
-  console.log(pokemon.types[0].type.name);
+  pnum.innerHTML = `<h1>Name: ${pokemon.name}</h1> 
+                    <h2> ID: #${pokemon.id}</h2> 
+                    <h3> Type: ${pokemon.types[0].type.name} ${pokemon.types[1]? ", "+pokemon.types[1].type.name : " "}<\h3>`;
+  //console.log(pokemon.types);
 }
